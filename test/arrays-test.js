474e5e18a34628c228a67ed2574d5f262393e7cc
Aslan1st <aslan1st@mac.com>
2017-10-01 02:54:40 +0000

    automatically backed up by learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -162,3 +162,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
