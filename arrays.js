bb2b3a8de70020c7d7b3e97946b60497781f5f37
Aslan1st <aslan1st@mac.com>
2017-10-01 02:34:32 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,23 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+console.log(chocolateBars.length);
+
+function addElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+
+}
+function destructivelyAddElementToBeginningOfArray(){
+
+}
+function addElementToEndOfArray(){
+
+}
+function destructivelyAddElementToEndOfArray(){
+
+}
+function accessElementInArray()
